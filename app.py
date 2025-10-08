c164c118e28a293a99ebc4f84ad21f760d1073b1
Cassandra Cinzori <cassiecinzori@Cassandras-MacBook-Pro.local>
2025-10-08 16:36:36 -0400

    Added TJ and store data

--- app.py
+++ app.py
@@ -24,6 +24,19 @@ line_colors = {
 }
 
 
+food_retail = gpd.read_file("data/Food_Data/food_retail.shp")
+convenience_stores = food_retail[food_retail['store_type'] == "Convenience Stores, Pharmacies, and Drug Stores"]
+grocery_stores = food_retail[food_retail['store_type'] == "Supermarket or Other Grocery"]
+
+
+
+trader_joes = gpd.read_file("data/Food_Data/trader_joes.shp")
+boston_zips = [2116, 2115, 2446, 2139, 2494, 2476, 2465, 2138, 1803, 1906, 1960]
+boston_tjs = trader_joes[trader_joes['postal_cod'].isin(boston_zips)]
+
+
+
+
 # CALLBACKS
 def create_folium_map(
     show_lines=True, show_stations=True, show_dorms=True, selected_lines=None
